8085453333f3906fee6291b032b75b822471bde2
yudhapakel <yudahp23@gmail.com>
2025-03-23 17:46:21 +0700

    menambahkan class SayaTubeVideo

---
+++ TPMODUL06_103022300010/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Video ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Yudha Harwanto");
+        video.PrintVideoDetails();
+    }
+}
